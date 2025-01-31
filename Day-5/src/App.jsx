6b31ab236f6cd6a1cb24c4f87c4a95e90a035f00
Rajendra Jat <jatrj18@gmail.com>
2025-01-31 14:15:21 +0530

    Day-5 : Color Box Generator

---
+++ Day-5/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ColorBoxGenerator from './components/ColorBoxGenerator';
+
+function App() {
+
+  return (
+    <>
+      <ColorBoxGenerator />
+    </>
+  )
+}
+
+export default App
